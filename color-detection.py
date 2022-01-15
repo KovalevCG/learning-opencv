4d09afa02df3676a5440768a843bef3741ac7bb1
Alexander Kovalev <kovalev.cg@gmail.com>
2022-01-15 22:29:49 +0200

    commit

--- color-detection.py
+++ color-detection.py
@@ -5,9 +5,11 @@ import numpy as np
 def empty(a):
     pass
 
-# path = 'assets/pickup.jpg'
+# Reading Image
 path = 'assets/menu.jpg'
 img = cv2.imread(path)
+
+# Trackbars
 cv2.namedWindow("Settings")
 cv2.resizeWindow("Settings", 340, 240)
 cv2.createTrackbar("Hue Min", "Settings", 0, 179, empty)
@@ -17,12 +19,10 @@ cv2.createTrackbar("Sat Max", "Settings", 255, 255, empty)
 cv2.createTrackbar("Val Min", "Settings", 0, 255, empty)
 cv2.createTrackbar("Val Max", "Settings", 255, 255, empty)
 
-
 # Downscaling Image
 x = int(img.shape[0]/3)
 y = int(img.shape[1]/3)
 img = cv2.resize(img, (y ,x))
-
 
 while True:
     imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
@@ -32,7 +32,6 @@ while True:
     s_max = cv2.getTrackbarPos("Sat Max", "Settings")
     v_min = cv2.getTrackbarPos("Val Min", "Settings")
     v_max = cv2.getTrackbarPos("Val Max", "Settings")
-    print(h_min)
     lower = np.array([h_min, s_min, v_min])
     upper = np.array([h_max, s_max, v_max])
     mask = cv2.inRange(imgHSV, lower, upper)
