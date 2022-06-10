8d5d88106e4271110d6c7fd7e513fb1da7b8bdd5
Alexander Kovalev <viznum.cg@gmail.com>
2022-06-11 01:40:28 +0300

    remove files

--- histogram.py
+++
@@ -1,8 +0,0 @@
-import cv2
-from matplotlib import pyplot as plt
-import numpy as np
-
-img = cv2.imread("assets/lp-city.jpg")
-img = cv2.resize(img, (700, 712))
-cv2.imshow("Image", img)
-cv2.waitKey(0)
